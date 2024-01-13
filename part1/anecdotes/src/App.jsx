b617cb5dc25833b16d9a196aac2803c533e39920
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2024-01-13 08:02:07 -0800

    Exercise 1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,13 +11,36 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
+  // create an initialVotes object with keys 0 to array length -1
+  // and all values equal to zero
+  const initialVotes = () => {
+    let voteObject = {}
+    for (let i=0; i<anecdotes.length; i++) {
+      voteObject[i] = 0
+    }
+    return voteObject
+  }
+
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(initialVotes())
+  console.log(selected)
+  console.log(votes)
+
   const getRandomInt = (max) => Math.floor(Math.random() * max)
+
+  const updateVotes = () => {
+    const newVotes = {
+      ...votes,
+      [selected]: votes[selected] + 1
+    }
+    setVotes(newVotes)
+  }
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <button onClick={updateVotes}>vote</button>
       <button onClick={() => setSelected(getRandomInt(anecdotes.length))}>next anecdote</button>
     </div>
   )
