700f3a4a639e9d5069f9c8deafe76b593860f9a6
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2024-01-20 05:49:15 -0800

    Exercise 2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 
 import personsService from './services/persons'
+import Notification from './components/Notfication'
 import Filter from './components/Filter'
 import AddForm from './components/AddForm'
 import DisplayPersons from './components/DisplayPersons'
@@ -12,6 +13,7 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [message, setMessage] = useState(null)
   
   // fetch initial data
   useEffect(() => {
@@ -34,19 +36,21 @@ function App() {
   const addPerson = (event) => {
     event.preventDefault()
     let existingPerson = persons.find(person => person.name === newName)
-    console.log("existing", existingPerson)
     if (existingPerson) {
       if (window.confirm(`${existingPerson.name} is already added to phonebook,
         replace the old number with this new one?`)) {
           const changedPerson = { ...existingPerson, number: newNumber}
-          console.log("new", changedPerson)
-          console.log("id", changedPerson.id)
           personsService
           .update(`${changedPerson.id}`, changedPerson)
-          .then(() => 
+          .then(() => {
             personsService
             .getAll()
-            .then(response => setPersons(response)))
+            .then(response => setPersons(response))
+            setMessage(`${existingPerson.name}'s number has been changed to ${changedPerson.number}.`)
+            setTimeout(() => {
+              setMessage(null)
+            }, 3000)
+          })
       }
     } else {
       const personObject = {
@@ -58,19 +62,29 @@ function App() {
       .then(response => {
         setPersons(persons.concat(response))
       })
+      setMessage(`Added ${personObject.name}`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 3000)
     }
     setNewName('')
     setNewNumber('')
   }
   const removePerson = (id) => {
-    if (window.confirm(`Delete ${persons.filter(person => 
-        person.id === id)[0].name}?`)) {
+    const personToRemove = persons.filter(person =>
+      person.id === id)[0].name
+    if (window.confirm(`Delete ${personToRemove}?`)) {
       personsService
       .remove(id)
-      .then(() => 
+      .then(() => {
         personsService
         .getAll()
-        .then(response => setPersons(response)))
+        .then(response => setPersons(response))
+        setMessage(`Removed ${personToRemove}`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 3000)
+      })
     }
   }
 
@@ -88,6 +102,9 @@ function App() {
   return (
     <div>
       <h1>Phonebook</h1>
+      <Notification
+        message={message}
+      />
       <Filter
         searchTerm={searchTerm}
         handleSearchChange={handleSearchChange}
---
+++ part2/phonebook/src/components/Notfication.jsx
@@ -0,0 +1,23 @@
+const Notification = ({ message }) => {
+
+  const messageStyle = {
+    color: "green",
+    background: "lightgrey",
+    fontSize: "20px",
+    borderStyle: "solid",
+    borderRadius: "5px",
+    padding: "10px",
+    marginBottom: "10px",
+  }
+
+  if (message === null) {
+    return null
+  }
+  return (
+    <div style={messageStyle}>
+      {message}
+    </div>
+  )
+}
+
+export default Notification
